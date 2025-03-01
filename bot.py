4140ea499c321f210a79bb8f0c6ba7b22b77da2f
Rfgfgfgfgf <rfgfgfgfgf3456787@gmail.com>
2025-03-01 21:20:24 +0400

    ми

--- bot.py
+++ bot.py
@@ -7,27 +7,31 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я Шлепа бот. Мой создатель - rfgfgfgfgf. Мои команды: /help, /start, /coin, /weather /help, /start, /command_text")
+    bot.reply_to(message, "Привет! Мой создатель - rfgfgfgfgf. Мои команды: /help, /start, /command_text")
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
     bot.reply_to(message, 'Привет, проверь мои команды: /help, /start, /command_text')
     
-@bot.message_handler(func=lambda message:True)
-def command_text(message):
-    promt = message.text
+@bot.message_handler(commands=['generate'])
+def generate_image(message):
+    prompt = message.text.replace('/generate', '')
 
+    if not prompt:
+        bot.reply_to(message, "Пожалуйста, введите описание изображения после команды /generate.")
+        return
+
+    bot.reply_to(message, "Генерирую изображение, подождите...")
 
     api = Text2ImageAPI('https://api-key.fusionbrain.ai/', API_KEY, SECRET_KEY)
     model_id = api.get_model()
-    uuid = api.generate(promt, model_id)
+    uuid = api.generate(prompt, model_id)
     images = api.check_generation(uuid)[0]
 
-    file_path = 'decoded_image.jpg'
-    api.save_image(images,file_path)
+    file_path = 'generated_image.jpg'
+    api.save_image(images, file_path)
 
-
-    with open(file_path,'rb') as photo:
+    with open(file_path, 'rb') as photo:
         bot.send_photo(message.chat.id, photo)
 
 # Запускаем бота
